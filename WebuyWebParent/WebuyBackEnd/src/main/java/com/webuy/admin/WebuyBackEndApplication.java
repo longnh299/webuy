5f00fe1a9574a8f8a572855f6212083a65e2e837
longnh299 <longkakapro2000@gmail.com>
2023-06-10 17:51:03 +0700

    setup structure of project

---
+++ WebuyWebParent/WebuyBackEnd/src/main/java/com/webuy/admin/WebuyBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.webuy.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebuyBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebuyBackEndApplication.class, args);
+	}
+
+}
---
+++ WebuyWebParent/WebuyFrontEnd/src/main/java/com/webuy/site/WebuyFrontEndApplication.java
@@ -0,0 +1,13 @@
+package com.webuy.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebuyFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebuyFrontEndApplication.class, args);
+	}
+
+}
