94c41d1c8f8d17949f32f8a47f267437b5a8c14a
longnh299 <longkakapro2000@gmail.com>
2023-06-14 22:23:27 +0700

    add new user function and interface

--- WebuyWebParent/WebuyBackEnd/src/main/java/com/webuy/admin/user/UserController.java
+++ WebuyWebParent/WebuyBackEnd/src/main/java/com/webuy/admin/user/UserController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.webuy.common.entity.Role;
 import com.webuy.common.entity.User;
 
 
@@ -24,4 +27,28 @@ public class UserController {
 		model.addAttribute("listUsers", listUsers);
 		return "users";
 	}
+	
+	//create new user controller
+	@GetMapping("/users/new")
+	public String createNewUser(Model model) {
+		List<Role> listRoles = userService.listRoles();
+		User newUser = new User();
+		newUser.setEnabled(true);
+		model.addAttribute("newUser", newUser);
+		model.addAttribute("listRoles", listRoles);
+		return "new_user_form";
+	}
+	
+	//save user controller
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		userService.save(user);
+		
+		redirectAttributes.addFlashAttribute("message", "The user has been saved successfully!");
+		return "redirect:/users";
+	}
+	
+	
+	
 }
--- WebuyWebParent/WebuyBackEnd/src/main/java/com/webuy/admin/user/UserService.java
+++ WebuyWebParent/WebuyBackEnd/src/main/java/com/webuy/admin/user/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.webuy.common.entity.Role;
 import com.webuy.common.entity.User;
 
 @Service
@@ -13,9 +14,24 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private RoleRepository roleRepository;
+	
 	//list all users service
 	public List<User> listAllUsers(){
 		return (List<User>) userRepository.findAll();
+	}
+	
+	//get all roles in db
+	public List<Role> listRoles(){
+		
+		return (List<Role>) roleRepository.findAll();
+		
+	}
+	
+	//save user into db
+	public void save(User user) {
+		userRepository.save(user);
 	}
 
 }
