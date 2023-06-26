c744c7810459b403f6b5782295530d67ce5cb26e
longnh299 <longkakapro2000@gmail.com>
2023-06-26 22:10:51 +0700

    add form login

--- WebuyWebParent/WebuyBackEnd/src/main/java/com/webuy/admin/security/SecurityConfig.java
+++ WebuyWebParent/WebuyBackEnd/src/main/java/com/webuy/admin/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,11 +19,23 @@ public class SecurityConfig {
 	}
 	
 	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**", "/webjars/**");
+	}
+	
+	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 	http
-	.authorizeHttpRequests((authz) -> authz
-	.anyRequest().permitAll()
-	);
+	.authorizeHttpRequests()
+	.anyRequest()
+	.authenticated()
+	.and()
+	.formLogin()
+		.loginPage("/login")
+		.usernameParameter("email")
+		.permitAll();
 	return http.build();
 	}
+	
+	
 }
